35667d74883ed6f794cbaf6364b5760d5de7c05a
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-01-30 10:35:54 +0100

    Init Repo

---
+++ wp-content/plugins/tf-nrw-2023/tf-nrw-2023.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Tamara Funiciello NRW 2023
+ * Version: 1.0.0
+ * Plugin URI:
+ * Description: Plugin for the NRW 2023 campaign of Tamara Funiciello
+ * Author: Timothy@K.
+ * Author URI: https://github.com/timothyoesch
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: tf-nrw-2023
+ *
+ * @package WordPress
+ * @author Timothy@K.
+ * @since 1.0.0
+ */
